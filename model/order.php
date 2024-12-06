8630270740f714704f1dbfa01360d5753a596c2a
quang truong <truonghqph23970@fpt.edu.vn>
2024-12-06 15:48:07 +0700

    Danh sách đơn hàng

--- model/order.php
+++ model/order.php
@@ -4,3 +4,13 @@ function create_order($user_id, $name, $total_amount, $shipping_address, $phone)
     $sql = "INSERT INTO orders (user_id, name, total_amount, shipping_address, phone) VALUES (?, ?, ?, ?, ?)";
     return pdo_execute($sql, $user_id, $name, $total_amount, $shipping_address, $phone);
 }
+
+function loadall_order() {
+    $sql = "select * from orders order by id desc";
+    return pdo_query($sql);
+}
+
+function loadone_order($order_id) {
+    $sql = "select * from orders where id = ?";
+    return pdo_query_one($sql, $order_id);
+}
--- model/order_detail.php
+++ model/order_detail.php
@@ -2,5 +2,15 @@
 
 function create_order_detail($order_id, $product_id, $quantity, $price, $total) {
     $sql = "INSERT INTO order_details (order_id, product_id, quantity, price, total) VALUES (?, ?, ?, ?, ?)";
-    pdo_execute($sql, $order_id, $product_id, $quantity, $price, $total);
+    return pdo_execute($sql, $order_id, $product_id, $quantity, $price, $total);
+}
+
+function load_order_detail($order_id) {
+    $sql = "SELECT d.product_id, d.quantity, d.price, d.total, p.name, p.image
+            FROM order_details d
+            JOIN sanpham p ON d.product_id = p.id
+            JOIN orders o ON d.order_id = o.id
+            WHERE o.id = ?";
+
+    return pdo_query($sql, $order_id);
 }
